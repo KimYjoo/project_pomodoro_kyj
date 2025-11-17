2ac979c96007c0c755bf2379fd76aa385f07fff4
KimYjoo <pas4976@gmail.com>
2025-11-17 15:08:21 +0900

    feat: Add timer mode Change feature
    - 버튼 클릭 시 미리 설정된 "휴식", "집중" 시간으로 설정이 바뀌는 기능

--- pomodoro/pages/timer/index.js
+++ pomodoro/pages/timer/index.js
@@ -4,25 +4,42 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { changeMicroToSecond, timeFormatMMSS } from "@/utils/timeUtils.js";
 import Timer from "@/domain/Timer";
 
+const TIMER_MODE_CONFIG = {
+    rest: "rest",
+    focus: "focus",
+};
+
 export default function TimerPage() {
-    const [durationSec, setDurationSec] = useState(100);
-    const [remainingSec, setRemainingSec] = useState(durationSec);
+    const [mode, setMode] = useState("focus");
+
+    const [focusDuration, setFocusDuration] = useState(3000);
+    const [restDuration, setRestDuration] = useState(600);
+
+    const [remainingSec, setRemainingSec] = useState(0);
 
     const timerRef = useRef(null);
     const timerWorkerUrl = "/worker/timerWorker.js";
 
     useEffect(() => {
-        const timer = new Timer(timerWorkerUrl);
-        timerRef.current = timer;
-        timer.setup(durationSec);
-        const offTick = timer.onTick((remainingMs) => {
+        timerRef.current = new Timer(timerWorkerUrl);
+        const offTick = timerRef.current?.onTick((remainingMs) => {
             setRemainingSec(changeMicroToSecond(remainingMs));
         });
         return () => {
             offTick();
-            timer.dispose();
+            timerRef.current.dispose();
         };
-    }, [timerWorkerUrl, durationSec]);
+    }, [timerWorkerUrl]);
+
+    useEffect(() => {
+        const duration = getDurationByMode();
+        setRemainingSec(duration);
+        timerRef.current?.setup(duration);
+    }, [mode, focusDuration, restDuration]);
+
+    const getDurationByMode = () => {
+        return mode === "focus" ? focusDuration : restDuration;
+    };
 
     const onClickStart = () => {
         timerRef.current?.start();
@@ -33,6 +50,9 @@ export default function TimerPage() {
     const onClickReset = () => {
         timerRef.current?.reset();
     };
+    const onClickModeChange = () => {
+        setMode(mode === "focus" ? "rest" : "focus");
+    };
 
     return (
         <>
@@ -40,6 +60,7 @@ export default function TimerPage() {
             <button onClick={onClickStart}>시작</button>
             <button onClick={onClickPause}>정지</button>
             <button onClick={onClickReset}>초기화</button>
+            <button onClick={onClickModeChange}>모드변경</button>
         </>
     );
 }
